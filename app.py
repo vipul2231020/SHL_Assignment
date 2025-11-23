a9374af9c77dd1907ac161f9de6a5e35867b11d9
Vipul <vipul2231020@akgec.ac.in>
2025-11-23 15:59:32 +0530

    modify streamlit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000/recommend"
+API_URL = "https://shl-recommend-api.onrender.com/recommend"
 
 st.set_page_config(
     page_title="SHL Assessment Recommendation System",
